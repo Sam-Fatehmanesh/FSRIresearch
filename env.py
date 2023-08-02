3f9f3f3527d16877cd7a062e7cf5742025903688
Sam-Fatehmanesh <sam.fatehmanesh@gmail.com>
2023-08-02 15:16:01 -0700

    cleaned env.py

--- env.py
+++ env.py
@@ -1,27 +1,6 @@
 import gymnasium as gym
-env = gym.make("LunarLander-v2", render_mode="human")
-observation, info = env.reset(seed=42)
-for _ in range(1000):
-   action = env.action_space.sample()  # this is where you would insert your policy
-   observation, reward, terminated, truncated, info = env.step(action)
+import gym_bandits
 
-   if terminated or truncated:
-      observation, info = env.reset()
-
-env.close()
-
-# class rlworld():
-#     def                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   import gymnasium as gym
-# env = gym.make("LunarLander-v2", render_mode="human")
-# observation, info = env.reset(seed=42)
-# for _ in range(1000):
-#    action = env.action_space.sample()  # this is where you would insert your policy
-#    observation, reward, terminated, truncated, info = env.step(action)
-
-#    if terminated or truncated:
-#       observation, info = env.reset()
-
-# env.close()
 
 class rlworld():
     def __init__(self, env_name, agent=None, render_mode=None):
@@ -71,6 +50,59 @@ class rlworld():
     
 
 
-w = rlworld("LunarLander-v2", render_mode="human")
+# w = rlworld("LunarLander-v2", render_mode="human")
 
-w.printenvstate()
+# w.printenvstate()
+
+class maBanditWorld():
+    def __init__(self, env_name, agent=None):
+        self.env = gym.make(env_name)
+        self.observation = self.env.reset()
+        self.reward = None
+        self.terminated = None
+        self.truncated = None
+        self.agent = None
+        self.actions = []
+        self.episode_Q = 0
+        self.observations = []
+        self.info = None
+
+
+    def printAS(self):
+        print(self.env.action_space)
+
+    def printenvstate(self):
+        print(self.observation)
+
+    def getObservation(self):
+        return self.observation
+
+    def step(self):
+        action = self.agent.getAction(self.observation)
+        self.actions.append(action)
+
+        self.observation, self.reward, self.terminated, self.truncated, self.info = self.env.step(action)
+        self.observations.append(self.observation)
+
+    def run(self, step_count, training_mode=False):
+        for _ in range(step_count):
+            self.step() 
+            if self.terminated or self.truncated:
+
+                self.observation = self.env.reset()
+                
+                if training_mode:
+                    self.agent.train(self.observations, self.actions, self.episode_Q)
+                    self.episode_Q = 0
+                    self.actions = []
+
+
+        env.close()
+
+    
+
+
+# w = maBanditWorld("BanditTenArmedGaussian-v0")#, render_mode="human")
+
+# w.printenvstate()
+# w.printAS()
--- models.py
+++ models.py
@@ -2,6 +2,7 @@ import sklearn
 from sklearn import linear_model
 import torch
 from sklearn import svm
+from sklearn.gaussian_process import GaussianProcessRegressor
 
 
 
@@ -16,14 +17,19 @@ class Ensemble:
     def raw_inf(self, X):
         return [model.predict(X) for model in self.models]
 
-    # returns mean, and variance of the mean
+    # returns mean and variance of the mean
     def predict(self, X):
-        return sum(self.raw_inf(X)) / len(self.models), sum([(self.raw_inf(X)[i] - self.predict(X)[0])**2 for i in range(len(self.models))]) / len(self.models)
+        outputs = self.raw_inf(X)
+        mean = sum(self.raw_inf(X)) / len(self.models)
+        return mean, sum([(outputs[i] - mean)**2 for i in range(len(self.models))]) / len(self.models)
     
 
-def get_linear_ensemble(model_count, seed=42):
+def get_linear_ensemble(model_count):
     return Ensemble(linear_model.LinearRegression, model_count)
 
-def get_svm_ensemble(model_count, seed=42):
+def get_svm_ensemble(model_count):
     return Ensemble(svm.SVC, model_count)
+
+def get_GP_ensemble(model_count):
+    return Ensemble(GaussianProcessRegressor, model_count)
 
