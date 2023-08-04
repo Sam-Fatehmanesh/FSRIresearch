20714bc5950b40e5cdf03fb1d43c29b5d9e3ea46
Brolsen2232 <35857348+Brolsen2232@users.noreply.github.com>
2023-08-03 18:28:24 -0700

    Debugging changes incomplete

--- thompson_samp.py
+++ thompson_samp.py
@@ -1,9 +1,9 @@
 # from env import environment
 from collections import defaultdict
 import numpy as np
-import gym
+import gymnasium
 import plotting
-import env as rlworld
+import env as maBandaWorld
 import random
 
 #expsil
@@ -35,20 +35,20 @@ class ThompsonSampling:
             if (episode_idx + 1) % 10 == 0:
                 print("\nEpisode {}/{}".format(episode_idx + 1, num_episodes))
             
-            observation = self.env.reset()
+            observation = self.env.env.reset()
             done = False
             time = 1
             
             while not done:
-                numBandits = self.env.action_space.n
+                numBandits = self.env.env.action_space.n
 
                 action = self.thompson_policy(episode_rewards, numBandits)
                 
-                next_observation, reward, done, _ = self.env.step(action)
+                next_observation, reward, done, _ = self.env.env.step(action)
                                 
                 # Update episode statistics
 
-                statistics.episode_rewards[episode_idx] += reward
+                statistics.episode_rewards[episode_idx] = reward
                 statistics.episode_lengths[episode_idx] = time
 
                 if done:
