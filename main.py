0f9472cd96816210d7bcacc04df4baf79636a131
Brolsen2232 <35857348+Brolsen2232@users.noreply.github.com>
2023-08-07 14:59:09 -0700

    Working version

--- main.py
+++ main.py
@@ -11,20 +11,13 @@ colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'orange', 'purple', 'brown']
 
 # Epsilon tuning
 
-for i in range(10):
-    ent_stats = max_ent.train(num_episodes=1000, learning_rate=0.1, discount_factor=0.9, epsilon=0.1-i*0.005, step_count=1000)
-    plotting.plot_episode_stats(ent_stats, "Max Entropy Rewards", "epsilon tuning", color=colors[i])
-
+ent_stats = max_ent.train(num_episodes=1000, learning_rate=0.16, discount_factor=0.9, epsilon=0.095, step_count=10000)
+plotting.plot_episode_stats(ent_stats, "Max Entropy Rewards", "epsilon tuning", color=colors[0])
+#EPSILON PREFERRED: 0.095
 # Learning rate tuning
-for j in range(10):
-    ent_stats = max_ent.train(num_episodes=1000, learning_rate=0.1+j*0.01, discount_factor=0.9, epsilon=0.1, step_count=1000)
-    plotting.plot_episode_stats(ent_stats, "Max Entropy Rewards", "learning rate tuning", color=colors[j])
+#0.16 LEARNING RATE
 
-# Discount factor tuning
-for k in range(10):
-    ent_stats = max_ent.train(num_episodes=1000, learning_rate=0.1, discount_factor=0.9+k*0.01, epsilon=0.1, step_count=1000)
-    plotting.plot_episode_stats(ent_stats, "Max Entropy Rewards", "discount factor tuning", color=colors[k])
-
+#0.9 DISCOUNT FACTOR
 plt.show()
 #thomp_stats = thomps.train(num_episodes=10000, step_count=1000)
 
