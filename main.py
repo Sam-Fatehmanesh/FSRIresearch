21ff4367872ffff2f49730f75f3121f4d863b880
jinhzhou <jzhou2@caltech.edu>
2023-08-08 18:10:18 -0700

    plotting changes on epi greed :D

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ thomps = ThompsonSampling(env)
 colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'orange', 'purple', 'brown']
 
 # Epsilon tuning
-for i in range(10):
-    ent_stats = max_ent.train(num_episodes=1000, learning_rate=0.16, discount_factor=0.9, epsilon=0.01, step_count=100, decay_factor=0.98)
-    plotting.plot_episode_stats(ent_stats, "Max Entropy Rewards", "epsilon tuning", color=colors[i])
+#for i in range(10):
+    #ent_stats = max_ent.train(num_episodes=1000, learning_rate=0.16, discount_factor=0.9, epsilon=0.01, step_count=100, decay_factor=0.98)
+    #plotting.plot_episode_stats(ent_stats, "Max Entropy Rewards", "epsilon tuning", color=colors[i])
 #EPSILON PREFERRED: 0.095
 # Learning rate tuning
 #0.16 LEARNING RATE
@@ -20,4 +20,8 @@ for i in range(10):
 
 #plotting.plot_episode_stats(thomp_stats, "Thompson Smapling Rewards ")
 #0.9 DISCOUNT FACTOR
+
+
+
+epsilon_greedy.train(num_episodes=1000, decay_rate=.005, epsilon = 1, step_count = 100)
 plt.show()
