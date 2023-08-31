4c1241489697050f1289e9219a42dc04503ca93d
Sam-Fatehmanesh <sam.fatehmanesh@gmail.com>
2023-08-31 15:08:44 -0700

    Added Thompson Sa

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 from maximum_entropy import MaxEntropyQLearning
 from thompson_samp import ThompsonSampling
+from otsFO import OptimisticThompsonSampling
 import plotting
 import numpy as np
 from env import maBanditWorld
 from matplotlib import pyplot as plt
 from epsilon_greedy import epsilon_greedy
 from ucb import upper_confidence_bound
-from OTS import OptimisticThompsonSampling
+from tqdm import tqdm
 
 env = maBanditWorld()
 max_ent = MaxEntropyQLearning(env)
-thomps = ThompsonSampling(env)
+thomps = OptimisticThompsonSampling(env)
 eps_greed = epsilon_greedy(10, env, 42)
 upp_con = upper_confidence_bound(10, env, 42)
 
-ots = OptimisticThompsonSampling(env)
-
 colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'orange', 'purple', 'brown']
-
+# thomps.train(1, 2000, .1, "red")
 # Epsilon tuning
 #for i in range(10):
     #ent_stats = max_ent.train(num_episodes=1000, learning_rate=0.16, discount_factor=0.9, epsilon=0.01, step_count=100, decay_factor=0.98)
@@ -25,25 +24,33 @@ colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'orange', 'purple', 'brown']
 #EPSILON PREFERRED: 0.095
 # Learning rate tuning
 #0.16 LEARNING RATE
-#thomp_stats = thomps.train(num_episodes=1, step_count=10000)
-#sthomps.plots()
-ent_stats = max_ent.train(num_episodes=2000, learning_rate=0.1, discount_factor=0.9, epsilon=0.01, step_count=1, decay_factor=0.98)
-max_ent.plots()
-# plotting.plot_episode_stats(thomp_stats, "Thompson Smapling Rewards ")
+#thomp_stats = thomps.train(num_episodes=10000, step_count=1000)
+
+#plotting.plot_episode_stats(thomp_stats, "Thompson Smapling Rewards ")
 #0.9 DISCOUNT FACTOR
 
+regret_curves = []
+for i in tqdm(range(512)):
+    np.random.seed(i)
+    # max_ent = MaxEntropyQLearning(env, seed=i+69)
 
-#eps_greed.train(num_episodes=1, decay_rate=.005, epsilon = 1, step_count = 10000)
-#eps_greed.plots()
+    # regret_curves.append(max_ent.train(num_episodes=1, learning_rate=0.16, discount_factor=0.9, epsilon=0.01, step_count=400, decay_factor=0.98))
+    thomps = OptimisticThompsonSampling(env, seed=i+69)
+    regret_curves.append(thomps.train(num_episodes=1, step_count=400, lambdaconst=0.1, graph_color=colors[i%10]))
+
+    
+
+
+for i in range(len(regret_curves)):
+    plt.plot([i for i in range(len(regret_curves[i]))], regret_curves[i], label = str(i))
+plt.show()
 
 #c is the confidence interval, higher c more explore, low c more exploit
 #epsilons = []
 #for j in range(30):
-    #epsilons.append(#upp_con.train(num_episodes=1, step_count= 4000, c=.99))
-#upp_con.train(num_episodes=1, step_count= 2000, c=.99)
+    #epsilons.append(upp_con.train(num_episodes=1, step_count= 4000, c=.99))
 #upp_con.plots()
 #print("Averaged epsilon: ", np.average(epsilons) )
-#ots_sts = ots.train(1, 1000)
 
 #epg 1/t decay results in lowered regret compared to ucb under the same step_count and num_episodes
 
---
+++ otsFO.py
@@ -0,0 +1,160 @@
+# from env import environment
+from collections import defaultdict
+import numpy as np
+import gymnasium
+from plotting import *
+import env as maBandaWorld
+import random
+from tqdm import tqdm
+from matplotlib import pyplot as plt
+from scipy.linalg import fractional_matrix_power
+
+
+
+#expsil Maxmimzing the number of states visited or the next state transition, action/state joint distribution
+class OptimisticThompsonSampling:
+    def __init__(self, env, seed=42):
+        np.random.seed(seed)
+        self.env = env
+
+    def thompson_policy(self, reward_arr, V_t, param_hat_t):
+
+        # mean = np.array([reward_arr[i][0]/(reward_arr[i][0] + reward_arr[i][1]) for i in range(len(reward_arr))])
+
+        # samples_list = [0 for i in range(len(reward_arr))]
+
+        # for i in range(len(reward_arr)):
+        #     sample = np.random.beta(1 + reward_arr[i][0], 1 + reward_arr[i][1])
+        #     if  sample > mean[i]:
+        #         samples_list[i] = sample
+        #         mean[i] = sample
+        #     else:
+        #         samples_list[i] = mean[i]
+                
+        # return np.argmax(np.array(samples_list))
+        v_negrad = fractional_matrix_power(V_t, -0.5)
+        
+        samples_list = param_hat_t + (np.matmul(np.array([np.random.beta(1 + reward_arr[i][0], 1 + reward_arr[i][1]) for i in range(len(reward_arr))]), v_negrad) * np.random.normal())
+
+        # print("v_negrad")
+        #print((np.matmul(np.array([np.random.beta(1 + reward_arr[i][0], 1 + reward_arr[i][1]) for i in range(len(reward_arr))]), V_t) * np.random.normal()))
+        # print("param_hat_t")
+        # print(param_hat_t)
+        # print("samples_list")
+        # print(samples_list)
+        return np.argmax(samples_list)
+
+    def train(self, num_episodes, step_count, lambdaconst, graph_color):
+        # Initialize Q-value function and episode statistics
+        statistics = EpisodeStats(
+        episode_lengths=np.zeros(num_episodes),
+        episode_rewards=np.zeros(num_episodes),
+        step_reward_avg=np.zeros(num_episodes*step_count)) #added this
+        numBandits = self.env.env.action_space.n
+        data = np.zeros((numBandits, 2))
+        rewards = np.zeros(num_episodes*step_count)
+        armPulled = np.zeros(numBandits)
+        armPulled_t = []
+        V_t = np.identity(n=numBandits) * lambdaconst
+        param_hat_t = np.zeros(numBandits)
+        total_arm_r = np.zeros(numBandits)
+        self.sigma_regret = 0
+
+        self.array_with_mean_reward_of_each_arm = []
+        for i in range(len(self.env.env.r_dist)):
+            mean_reward_of_each_arm = np.mean(self.env.env.r_dist[i])
+            self.array_with_mean_reward_of_each_arm.append(mean_reward_of_each_arm)
+        
+        self.best_arm = np.argmax(self.array_with_mean_reward_of_each_arm)
+        self.mean_reward_of_best_arm = np.max(self.array_with_mean_reward_of_each_arm)
+        self.running_regret = []
+        self.steps = []
+        #self.maxreward = np.max(env
+
+        #optimistic_mean = np.ones(numBandits) * (-100000)
+        
+        
+
+
+        for episode_idx in tqdm(range(num_episodes), leave=True):
+            
+            observation = self.env.env.reset()
+            done = False
+            time = 1
+            action_reward_product_sum = np.zeros(numBandits)
+            
+            while not done:
+                for i in tqdm(range(step_count), leave=True):
+                    
+                    # print(len(statistics.episode_rewards))
+                    action = self.thompson_policy(data, V_t, param_hat_t)#, optimistic_mean)#self.thompson_policy(statistics.episode_rewards, numBandits)
+                    action_vec = np.zeros(numBandits)
+                    action_vec[action] += 1
+                    armPulled_t.append(action)
+                     
+                    
+                    
+                    next_observation, reward, done, _ = self.env.env.step(action)
+                    total_arm_r[action] += reward
+                    armPulled[action] += 1
+                    if reward > 0:
+                        data[action, 0] += 1
+                    else:
+                        data[action, 1] += 1
+
+                    V_t += np.matmul(action_vec, np.transpose(action_vec))
+                    param_hat_t = np.matmul(np.linalg.inv(V_t), action_reward_product_sum)
+                    action_reward_product_sum += action_vec * reward
+                    # print(action_reward_product_sum)R
+                                    
+                    # Update episode statistics
+                    rewards[episode_idx*step_count + i] = reward
+
+                    statistics.episode_rewards[episode_idx] += reward
+                    statistics.episode_lengths[episode_idx] = time
+                    statistics.step_reward_avg[episode_idx*step_count + i] = np.sum(rewards)/(episode_idx*step_count + i + 1)
+
+                    mean_reward_of_selected_arm = np.mean(self.env.env.r_dist[action])
+                    regret_of_step = self.mean_reward_of_best_arm - mean_reward_of_selected_arm
+                    self.sigma_regret += regret_of_step
+
+                    self.running_regret.append(self.sigma_regret)
+                    self.steps.append(len(self.steps))
+
+                    if done:
+                        done = True
+                    else:
+                        observation = next_observation
+                        time += 1
+
+        # cum_regret = np.zeros(num_episodes*step_count)
+        # r_dist = self.env.env.getRDist()
+        # p_dist = self.env.env.p_dist
+        # R = [r_dist[i][0] * p_dist[i] for i in range(numBandits)]
+        # # print("R")
+        # # print(R)
+        # regret = np.zeros(num_episodes*step_count)
+
+        # maxR = np.max(R)
+        # for i in tqdm(range(num_episodes*step_count)):
+        #     cum_regret[i] = ((i+1)*maxR) - R[armPulled_t[i]]
+        #     regret[i] = maxR - R[armPulled_t[i]]
+
+
+
+
+        # print("max R: " + str(maxR))
+        # plot_episode_stats(statistics, "Thompson Sampling", " ", "b")
+        # X = [i for i in range(num_episodes*step_count)]
+        # plotPointGraph(X, cum_regret, "Step", "Cumulative Regret", "Thompson Sampling Cumulative Regret", graph_color)
+        # plotPointGraph(X, regret, "Step", "Regret", "Thompson Sampling Regret", graph_color)
+        # plt.show()
+        return self.running_regret
+
+def plotPointGraph(X, Y, xlabel, ylabel, title, color, figsize=(50,50)):
+    fig = plt.figure(figsize=figsize)
+    plt.plot(X, Y, color = color)#, color = 'blue', width =.4)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    return fig
